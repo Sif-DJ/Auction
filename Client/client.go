1d8832bd72875c39b6303d081536b9b544cfbe02
TimelessLich <139888197+TimelessLich@users.noreply.github.com>
2024-11-19 12:07:01 +0100

    test

--- Client/client.go
+++ Client/client.go
@@ -4,25 +4,28 @@ import (
 	proto "Auction/grpc"
 	"context"
 	"log"
+	"strconv"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func main () {
+func main() {
 	address := "5050"
 	client := StartNode(address)
-	_, err := client.SendBid(context.Background(), &proto.Bid{Node: address, Amount: 0})
+	_, err := client.SendBid(context.Background(), &proto.Bid{Node: address, Amount: 500})
 	if err != nil {
 		log.Println("SendBid error: ", err)
 	}
-}
+	outcome, _ := client.Result(context.Background(), &proto.Empty{})
+	log.Println(" is auction done" + strconv.FormatBool(outcome.AuctionFinished))
 
+}
 
 func StartNode(address string) proto.AuctionServiceClient {
 	conn, err := grpc.NewClient("localhost:"+address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Exception Error")
 	}
-	return proto.NewAuctionServiceClient(conn)	
+	return proto.NewAuctionServiceClient(conn)
 }
